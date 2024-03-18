b87a4dfbd240b3f8ab25f2daaa1465ed596a1500
bluecrown5@ymobile.ne.jp <bluecrown2525@gmail.com>
2024-03-19 03:58:41 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <Novice.h>
+#include "MyMath/MatrixMath.h"
+
+const char kWindowTitle[] = "イシカワタケシ_MT3_01_00";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+	Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+	Matrix4x4 viewportMatrix = MakeVeiwportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		MatirxScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatirxScreenPrintf(0, 20 * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatirxScreenPrintf(0, 20 * 5 * 2, viewportMatrix, "viewportMatrix");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
